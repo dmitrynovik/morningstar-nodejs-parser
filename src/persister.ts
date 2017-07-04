76049a2ce956fe9a4b43b8ed3baed00e3ebe47f7
dmitrynovik <cryinstone@gmail.com>
2017-07-04 13:49:53 +1000

    truncate table if exists

--- src/persister.ts
+++ src/persister.ts
@@ -7,22 +7,20 @@ export default class Persister {
 
     private db : any;
 
-    constructor(private dbPath: string) {
+    constructor(private dbPath: string) {        
+        this.db = new sqlite.Database(this.dbPath);
+
         fs.exists(dbPath, (exists:boolean) => {
             if (exists) {
-                fs.unlink(dbPath);
-            };
-            this.createDatabase();
+                this.db.run("DELETE FROM holdings");
+            } else {
+                this.db.run("CREATE TABLE holdings (fundTicker TEXT, ticker TEXT, name TEXT, weight REAL, shares REAL, sector TEXT, style TEXT, firstBought TEXT, country TEXT, YTD REAL)");
+            }
         });
     }
 
-    createDatabase() {
-        this.db = new sqlite.Database(this.dbPath);
-        this.db.run("CREATE TABLE holdings (fundTicker TEXT, ticker TEXT, name TEXT, weight REAL, shares REAL, sector TEXT, style TEXT, firstBought TEXT, country TEXT, YTD REAL)");
-    }
-
     persist(fundTicker: string, h: Holding) {
-            this.db.run("INSERT INTO holdings VALUES (?,?,?,?,?,?,?,?,?, ?)", 
+            this.db.run("INSERT INTO holdings VALUES (?,?,?,?,?,?,?,?,?,?)", 
                 fundTicker,
                 h.ticker,
                 h.name,
